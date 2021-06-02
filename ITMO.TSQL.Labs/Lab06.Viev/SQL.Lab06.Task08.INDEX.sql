a7608304f997f774b90be67e1aa414c9b890d5d6
Георгий Исаев <georgiyelbaf@gmail.com>
2021-06-03 00:10:20 +0300

    add tsql.lab6

---
+++ ITMO.TSQL.Labs/Lab06.Viev/SQL.Lab06.Task08.INDEX.sql
@@ -0,0 +1,20 @@
+/*Вызов представления*/SELECT * FROM CustomerDetails.v_CustFinProducts
+/*Попытка изменить таблицу*/
+ALTER TABLE CustomerDetails.Customers 
+ALTER COLUMN CustomerFirstName nvarchar(100)/*Создать индекцсированное представление*/SET ANSI_NULLS ON
+GO
+SET ANSI_PADDING ON
+GO
+SET ANSI_WARNINGS ON
+GO
+SET CONCAT_NULL_YIELDS_NULL ON
+GO
+SET ARITHABORT ON
+GO
+SET QUOTED_IDENTIFIER ON
+GOSET NUMERIC_ROUNDABORT OFF
+GOSET ANSI_NULLS OFF
+GO
+SET QUOTED_IDENTIFIER OFF
+GOCREATE UNIQUE CLUSTERED INDEX IX_CustFinProdS
+ON CustomerDetails.v_CustFinProducts (AccountNumber, ProductName)
