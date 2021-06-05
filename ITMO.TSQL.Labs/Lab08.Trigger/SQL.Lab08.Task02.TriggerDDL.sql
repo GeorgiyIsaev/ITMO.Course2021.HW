b1ba1fb15e6a627dc1958c4f7b5f5dcab2cb0d67
Георгий Исаев <georgiyelbaf@gmail.com>
2021-06-05 23:24:38 +0300

    add tsql.lab8

---
+++ ITMO.TSQL.Labs/Lab08.Trigger/SQL.Lab08.Task02.TriggerDDL.sql
@@ -0,0 +1,36 @@
+/*Создание триггера DDL*/
+CREATE TRIGGER trgSprocs
+ON DATABASE
+FOR CREATE_PROCEDURE, ALTER_PROCEDURE, DROP_PROCEDURE
+AS
+IF DATEPART(hh, GETDATE()) > 9 AND DATEPART(hh, GETDATE()) < 17
+BEGIN
+	DECLARE @Message nvarchar(max)
+	SELECT @Message = 'Completing work during core hours. Trying to release -'
+		+ EVENTDATA().value('(/EVENT_INSTANCE/TSQLCommand/CommandText)[1]','nvarchar(max)')
+	RAISERROR (@Message, 16, 1)
+	ROLLBACK
+	EXEC msdb.dbo.sp_send_dbmail
+		@profile_name = 'SQL Server Database Mail Profile',
+		@recipients = 'exam@limtu.spb.ru',
+		@body = 'A stored procedure change',
+		@subject = 'A stored procedure change has been initiated
+		and rolled back during core hours'
+END;
+/*Тест срабатывания тригера*/
+CREATE PROC Test1
+AS
+SELECT 'Hello all';
+
+/*-----------*/
+/*триггер DDL срабатывающий при любом воздействии на базу*/
+CREATE TRIGGER trgDBDump
+ON DATABASE
+FOR DDL_DATABASE_LEVEL_EVENTS
+AS
+SELECT EVENTDATA();
+
+/*Тест тригера*/
+CREATE PROC Test2
+AS
+SELECT 'Hello all';
---
+++ ITMO.TSQL.Labs/Lab08.Trigger/SQL.Lab08.Task03.DropTriggerDDL.sql
@@ -0,0 +1,4 @@
+/*Удаление тригеров*/
+DROP TRIGGER trgSprocs ON DATABASE;
+
+DROP TRIGGER trgDBDump ON DATABASE;
