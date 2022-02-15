1f8ab6b70fe81370bcd11f137d024a55df182fe7
Георгий Исаев <georgiyelbaf@gmail.com>
2022-02-15 22:07:52 +0300

    email-forma для отправки

--- ITMO.HTML5.CCS3.JS/WebSite.Resume/js/main.js
+++ ITMO.HTML5.CCS3.JS/WebSite.Resume/js/main.js
@@ -19,24 +19,24 @@ $(document).ready(function(){
         console.log("inputs[0].value",  inputs[0].value);
         inputs[0].value
 
-        var fullAnket = "-- ОРГАНИЗАЦИЯ: \n";
-        fullAnket +="Название организации: " + inputs[0].value + "\n";
-        fullAnket +="Отдел: "+ inputs[1].value + "\n";
-        fullAnket +="Направление деятельность: "+ inputs[2].value + "\n";
-        fullAnket +="Город: "+ inputs[3].value + "\n";
-        fullAnket +=" -- ТРЕБОВАНИЯ: \n";
-        fullAnket +="Требуемый язык программировании: "+ inputs[4].value + "\n";
-        fullAnket +="Набор технологий: "+ inputs[5].value + "\n";
-        fullAnket +="Доп. наывыки: "+ inputs[6].value + "\n";
-        fullAnket +="-- КОНТАКТНАЯ ИНФОРМАЦИЯ: \n";
-        fullAnket +="E-mail: "+ inputs[7].value + "\n";
-        fullAnket +="Телефон: "+ inputs[8].value + "\n";
-        fullAnket +="ФИО рекурутера: "+ inputs[9].value + "\n";
+        var fullAnket = "<h2>ОРГАНИЗАЦИЯ: </h2>\n";
+        fullAnket +="<b>Название организации: </b>" + inputs[0].value + "</br>\n";
+        fullAnket +="<b>Направление деятельность: </b>"+ inputs[2].value + "</br>\n";
+        fullAnket +="<b>Отдел: </b>"+ inputs[1].value + "</br>\n";        
+        fullAnket +="<b>Город: </b>"+ inputs[3].value + "\n";
+        fullAnket +="<h2>ТРЕБОВАНИЯ: </h2>\n";
+        fullAnket +="<b>Требуемый язык программировании: </b>"+ inputs[4].value + "</br>\n";
+        fullAnket +="<b>Набор технологий: </b>"+ inputs[5].value + "</br>\n";
+        fullAnket +="<b>Доп. навыки: </b>"+ inputs[6].value + "</br>\n";
+        fullAnket +="<h2>КОНТАКТНАЯ ИНФОРМАЦИЯ:</h2>\n";
+        fullAnket +="<b>E-mail: </b>"+ inputs[7].value + "</br>\n";
+        fullAnket +="<b>Телефон: </b>"+ inputs[8].value + "</br>\n";
+        fullAnket +="<b>ФИО рекрутера:</b> "+ inputs[9].value + "</br>\n";
 
 
         console.log("fullAnket",  fullAnket);
         /*alert("Анкета отправлена: \n" + fullAnket);*/
-        saveToPC(fullAnket);
+       /* saveToPC(fullAnket);*/
         window.open('mailto:Georgiyelbaf@gmail.com?subject=Анктеа_от_Рекрутера&body=' + fullAnket);
 
     })
